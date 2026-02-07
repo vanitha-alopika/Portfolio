2148d27275cbc1b8348e4450034bb5ddfe2d6f0a
D VANITHA <vanithad859@gmail.com>
2026-02-07 17:13:41 +0530

    Refactor constants.js to update project details

--- constants.js
+++ constants.js
@@ -10,7 +10,7 @@ const PROJECTS = [
   {
     title: "Digital Banking Management System",
     description: "The Digital Banking Management System is an advanced Java-based application that enables users to securely manage bank accounts, perform transactions, view balance details, and maintain customer records. It supports features like login authentication, fund transfer, transaction history, and account management using JDBC and SQL.",
-    tech: ["Advanced Java", "JDBC", "SQL"],
+    tech: ["Advanced Java", "JDBC","Core Java","HTML","CSS","Servlet","JSP","JEE","MYSQL","Tomcat Server"],
     link: "https://github.com/vanitha-alopika/Digital-Banking-System",
     
   }
@@ -65,6 +65,7 @@ const CONTACT = {
     linkedin: "https://www.linkedin.com/in/d-vanitha-954651280?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
     github: "https://share.google/IERmsMweDi9Oiowmv" // User provided this link, assuming it redirects or is correct as per request
 };
+
 
 
 
